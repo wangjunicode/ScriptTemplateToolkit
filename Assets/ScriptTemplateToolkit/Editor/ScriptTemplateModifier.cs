1b63d4048a82e7649481f23d0c0b67adfd993ea5
Mogoson <mogoson@qq.com>
2017-10-12 22:45:33 +0800

    Format scripts.

--- Assets/ScriptTemplateToolkit/Editor/ScriptTemplateModifier.cs
+++ Assets/ScriptTemplateToolkit/Editor/ScriptTemplateModifier.cs
@@ -1,20 +1,13 @@
 /*************************************************************************
  *  Copyright (C), 2017-2018, Mogoson Tech. Co., Ltd.
- *  FileName: ScriptTemplateModifier.cs
- *  Author: Mogoson   Version: 0.1.0   Date: 8/31/2017
- *  Version Description:
- *    Internal develop version, mainly to achieve its function.
- *  File Description:
- *    Ignore.
- *  Class List:
- *    <ID>           <name>             <description>
- *     1.    ScriptTemplateModifier        Ignore.
- *  Function List:
- *    <class ID>     <name>             <description>
- *     1.
- *  History:
- *    <ID>    <author>      <time>      <version>      <description>
- *     1.     Mogoson     8/31/2017       0.1.0       Create this file.
+ *------------------------------------------------------------------------
+ *  File         :  ScriptTemplateModifier.cs
+ *  Description  :  Modifier for script template.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  8/31/2017
+ *  Description  :  Initial development version.
  *************************************************************************/
 
 using System;
--- README.md
+++ README.md
@@ -55,4 +55,4 @@
 ![TestScript](./Attachments/TestScript.png)
 
 ## Contact
-- If you have anny questions, fell free to contact me at mogoson@qq.com.
+- If you have any questions, fell free to contact me at mogoson@qq.com.
